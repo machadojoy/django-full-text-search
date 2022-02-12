63f2c4bce80b4c8d8c0ea5f1e029777fcc52300f
machadojoy <73057972+machadojoy@users.noreply.github.com>
2022-02-12 23:21:26 +0100

    adding additional search methodologies

--- book/views.py
+++ book/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from book.forms import PostSearchForm
 from book.models import Book
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, \
+    TrigramSimilarity, TrigramDistance, SearchHeadline
 
 
 # Standard textual (case-sensitive) queries
@@ -26,6 +27,38 @@ def search_rank(search_query):
     return Book.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
 
+# Search ranking weights
+def search_weights(search_query):
+    vector = SearchVector("title", weight="A") + SearchVector("authors", weight="B")
+    query = search_query
+    return Book.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
+
+# Search Trigram Similarity
+"""
+ Postgres SQL query for trigram similarity:
+     SELECT title, word_similarity(title, "harry potter") AS word_similarity
+     FROM book_book
+     WHERE title % "harry potter"
+     ORDER BY word_similarity DESC
+"""
+def trigram_similarity(search_query):
+    return Book.objects.annotate(similarity=TrigramSimilarity("title", search_query),).filter(similarity__gte=0.3)\
+        .order_by("-similarity")
+
+
+def trigram_distance(search_query):
+    return Book.objects.annotate(distance=TrigramDistance("title", search_query),).filter(ditance__lte=0.5)\
+        .order_by("distance")
+
+
+# Search Headline
+def search_headline(search_query):
+    vector = SearchVector("authors")
+    query = search_query
+    return Book.objects.annotate(search=vector, headline=SearchHeadline("authors", query)).filter(search=query)
+
+
 def post_search(request):
     form = PostSearchForm
 
@@ -34,5 +67,5 @@ def post_search(request):
         form = PostSearchForm(request.GET)
         if form.is_valid():
             search_query = form.cleaned_data["search"]
-            results = search_rank(search_query)
+            results = search_headline(search_query)
     return render(request, "index.html", {"form": form, "results": results, "search_query": search_query})
