b7e159228c71f108afebfe41c07d10e0b2c34f43
machadojoy <73057972+machadojoy@users.noreply.github.com>
2022-02-12 22:31:16 +0100

    adding some postgres search methods

--- book/views.py
+++ book/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
 from book.forms import PostSearchForm
 from book.models import Book
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+
+
+# Standard textual (case-sensitive) queries
+def contains(search_query):
+    return Book.objects.filter(title__icontains=search_query)
+
+
+# Full text postgres search
+def search(search_query):
+    return Book.objects.filter(title__search=search_query)
+
+
+# Postgres search vector against multiple fields
+def search_vector(search_query):
+    return Book.objects.annotate(search=SearchVector("title", "authors"),).filter(search=search_query)
+
+
+# Postgres search ranking
+def search_rank(search_query):
+    vector = SearchVector("title")
+    query = SearchQuery(search_query)
+    return Book.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+
 
 def post_search(request):
     form = PostSearchForm
@@ -10,5 +34,5 @@ def post_search(request):
         form = PostSearchForm(request.GET)
         if form.is_valid():
             search_query = form.cleaned_data["search"]
-            results = Book.objects.filter(title__contains=search_query)
-    return render(request, "index.html", {"form": form, "results": results})
+            results = search_rank(search_query)
+    return render(request, "index.html", {"form": form, "results": results, "search_query": search_query})
