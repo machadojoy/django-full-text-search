05a98d595da4a552033ceed7955485d44c72e4f3
machadojoy <73057972+machadojoy@users.noreply.github.com>
2022-02-12 23:43:28 +0100

    adding the gin index

--- book/models.py
+++ book/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils.translation import gettext as _
+from django.contrib.postgres.indexes import GinIndex
 
 
 class Book(models.Model):
     title = models.CharField(_("title"), max_length=1000, null=False, db_index=True)
     authors = models.CharField(_("authors"), max_length=1000)
+
+    class Meta:
+        indexes = [
+            GinIndex(name="GinIndex", fields=["title"])
+        ]
